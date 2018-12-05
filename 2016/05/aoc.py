926537b6777342ff3ad97c8946f3c865769956fc
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2018-12-04 21:13:18 -0500

    Day 05 part 2

--- 2016/05/aoc.py
+++ 2016/05/aoc.py
@@ -17,21 +17,57 @@ In this example, after continuing this search a total of eight times, the passwo
 Given the actual Door ID, what is the password?
 
 Your puzzle input is ffykfhsq.
+
+Your puzzle answer was c6697b55.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+As the door slides open, you are presented with a second door that uses a slightly more inspired security mechanism. Clearly unimpressed by the last version (in what movie is the password decrypted in order?!), the Easter Bunny engineers have worked out a better solution.
+
+Instead of simply filling in the password from left to right, the hash now also indicates the position within the password to fill. You still look for hashes that begin with five zeroes; however, now, the sixth character represents the position (0-7), and the seventh character is the character to put in that position.
+
+A hash result of 000001f means that f is the second character in the password. Use only the first result for each position, and ignore invalid positions.
+
+For example, if the Door ID is abc:
+
+The first interesting hash is from abc3231929, which produces 0000015...; so, 5 goes in position 1: _5______.
+In the previous method, 5017308 produced an interesting hash; however, it is ignored, because it specifies an invalid position (8).
+The second interesting hash is at index 5357525, which produces 000004e...; so, e goes in position 4: _5__e___.
+You almost choke on your popcorn as the final character falls into place, producing the password 05ace8e3.
+
+Given the actual Door ID and this new method, what is the password? Be extra proud of your solution if it uses a cinematic "decrypting" animation.
 """
 import sys
 import re
 import hashlib
+from tqdm import tqdm
 
 if __name__ == "__main__":
     INPUTSTR = "ffykfhsq"
     pw = []
     i = 0
-    while len(pw) < 8:
-        hd = hashlib.md5()
-        hd.update(INPUTSTR+str(i))
-        hd = hd.hexdigest()
-        if hd.startswith("00000"):
-            pw.append(hd[5])
-            print(pw)
-        i += 1
+    if len(sys.argv) < 2:
+        while len(pw) < 8:
+            hd = hashlib.md5()
+            hd.update(INPUTSTR+str(i))
+            hd = hd.hexdigest()
+            if hd.startswith("00000"):
+                pw.append(hd[5])
+                print(pw)
+            i += 1
+
+    else:
+        pw = [None] * 8
+        with tqdm(total=len(pw)) as pbar:
+            while None in pw:
+                hd = hashlib.md5()
+                hd.update(INPUTSTR+str(i))
+                hd = hd.hexdigest()
+                pos = int(hd[5], 16)
+                if hd.startswith("00000") and pos < len(pw) and pw[pos] is None:
+                    pw[pos] = hd[6]
+                    pbar.update(1)
+                i += 1
+
     print("".join(pw))
