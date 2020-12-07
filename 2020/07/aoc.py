54125b354aa8e0d00d60425ff2ba587141297449
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2020-12-07 08:23:47 -0500

    2020.7

---
+++ 2020/07/aoc.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+"""
+--- Day 7: Handy Haversacks ---
+You land at the regional airport in time for your next flight. In fact, it looks
+like you'll even have time to grab some food: all flights are currently delayed
+due to issues in luggage processing.
+
+Due to recent aviation regulations, many rules (your puzzle input) are being
+enforced about bags and their contents; bags must be color-coded and must
+contain specific quantities of other color-coded bags. Apparently, nobody
+responsible for these regulations considered how long they would take to
+enforce!
+
+For example, consider the following rules:
+
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+
+These rules specify the required contents for 9 bag types. In this example,
+every faded blue bag is empty, every vibrant plum bag contains 11 bags (5 faded
+blue and 6 dotted black), and so on.
+
+You have a shiny gold bag. If you wanted to carry it in at least one other bag,
+how many different bag colors would be valid for the outermost bag? (In other
+words: how many colors can, eventually, contain at least one shiny gold bag?)
+
+In the above rules, the following options would be available to you:
+
+A bright white bag, which can hold your shiny gold bag directly.
+A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
+A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+
+So, in this example, the number of bag colors that can eventually contain at
+least one shiny gold bag is 4.
+
+
+How many bag colors can eventually contain at least one shiny gold bag? (The
+list of rules is quite long; make sure you get all of it.)
+"""
+"""
+--- Part Two ---
+It's getting pretty expensive to fly these days - not because of ticket prices,
+but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+faded blue bags contain 0 other bags.
+dotted black bags contain 0 other bags.
+vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within
+it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2
++ 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper
+than this example; be sure to count all of the bags, even if the nesting becomes
+topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+"""
+
+import sys, itertools, numpy as np, re
+
+
+def parse_bag_list(bag_list):
+    """parse bag list portion of rule, return list of tuple (count, bag color)"""
+    if "no other bags" in bag_list:
+        return []
+    sub_bags = []
+    re_get_bag_color = re.compile(r"(\d+) ([\w ]+) bags?")
+    for bag in bag_list.split(','):
+        m = re_get_bag_color.search(bag)
+        assert m, "bleh"
+        sub_bags.append((int(m.group(1)), m.group(2)))
+    return sub_bags
+
+def parse(data):
+    """parse each "rule" line, building a dictionary of
+    {"bag color": <list of bags it contains>}
+    GRAPH BABAAY"""
+
+    bags = {}
+    re_get_outer_bag = re.compile(r"([\w ]+) bags contain (.*)\.$")
+    for line in data.splitlines():
+        m = re_get_outer_bag.match(line)
+        assert m, "ugh '{}'".format(line)
+        bags[m.group(1)] = parse_bag_list(m.group(2))
+
+    return bags
+
+def contains_shiny_gold(bag_dict, val):
+    """check if "shiny gold" is in this bag list (val), recurse"""
+    if "shiny gold" in [v[1] for v in val]:
+        return True
+    if not val:
+        return False
+    for _, v in val:
+        if contains_shiny_gold(bag_dict, bag_dict[v]):
+            return True
+    return False
+
+def count_bags(data):
+    count = 0
+    for v in data.values():
+        if contains_shiny_gold(data, v):
+            count += 1
+    return count
+
+def count_bags_part_2(baglist, val):
+    count = 1  # at least the current bag
+    for number, color in val:
+        count += number * count_bags_part_2(baglist, baglist[color])
+    return count
+
+def main():
+    # actual vals
+    vals = parse(open(sys.argv[1], "r").read())
+    # test vals
+    testdata = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."""
+    testvals = parse(testdata)
+
+    print(">>> part 1 <<<")
+    print(count_bags(testvals))
+    print(count_bags(vals))
+    print(">>> part 2 <<<")
+    print(count_bags_part_2(testvals, testvals["shiny gold"]) - 1)
+    print(count_bags_part_2(vals, vals["shiny gold"]) - 1)
+
+if __name__ == "__main__":
+    main()
